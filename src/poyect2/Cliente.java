49b5aa0111fbb67ca5c66e6628d7134fd07099d1
celemelez <cmezalezcano@americavirtualsa.com>
2024-05-28 11:21:37 -0300

    correccion

--- src/poyect2/Cliente.java
+++ src/poyect2/Cliente.java
@@ -2,9 +2,30 @@ package poyect2;
 
 public class Cliente extends Personas {
 	private int Clienteid;
+	
 
 	
 	
+	public int getClienteid() {
+		return Clienteid;
+	}
+
+
+
+	public void setClienteid(int clienteid) {
+		Clienteid = clienteid;
+	}
+	
+
+
+
+	@Override
+	public String toString() {
+		return "Cliente [Clienteid=" + Clienteid + "]";
+	}
+
+
+
 	public Cliente() {
 		super();
 	}
